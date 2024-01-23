4d56abca9893d538e92c535af1a6974c3b1a3af3
mascott82 <m.a.scott@live.com>
2024-01-23 09:51:58 -0600

    Featuring - Feature admin categories.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    resources :categories, only: [:index, :new, :create]
+  end
+  
   #get 'about/index'
   get 'about', to:  'about#index'
   root to: 'products#index'
